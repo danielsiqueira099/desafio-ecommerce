c56ca3e1e8de9b93f921b301365f49721051ce55
Daniel <daniel-nitinho@hotmail.com>
2021-06-28 23:49:56 -0300

    refac: adição dos métodos para confirmar compra e calcular preço

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,19 +1,48 @@
 import React, { useContext, useCallback } from 'react';
+import List from '@material-ui/core/List';
 
 import AplicationContext from '../../context';
 import './styles.css';
+import ListIcon from '../ListItem/index'
 
 const ModalCart = ({ title }) => {
-  const { setIsModalVisible, productsCar } = useContext(AplicationContext)
+  const { setIsModalVisible, productsCar, setSnackMessage, setIsSnackVisible } = useContext(AplicationContext)
 
   const handleCancel = useCallback(() => {
     setIsModalVisible(false);
   }, [setIsModalVisible])
+  
+  const handleConfirm = useCallback(() => {
+    setSnackMessage('Compra realizada com sucesso!')
+    setIsSnackVisible(true)
+    setIsModalVisible(false);
+  }, [setIsModalVisible, setSnackMessage, setIsSnackVisible])
+
+  const calculatePrice = useCallback(() => {
+    let price = 0
+    let productPrice = 0
+
+    productsCar.forEach(item => {
+      price = price + item.price + 10
+      productPrice += item.price
+    })
+
+    if (productPrice > 250) return `Preço total sem frete: R$ ${productPrice}` 
+
+    return `Preço total com frete: R$ ${price}`
+  }, [])
 
   return (
       <div className="modalBx">
         <div className="contentModal">
           <h2 className="title">{title}</h2>
+          <List className="listBx">
+            {productsCar.map(item => <ListIcon item={item} />)}
+          </List>
+          {
+            productsCar.length > 0 && 
+            <p>{calculatePrice()}</p>
+          }
           <div className="buttonBx">
             <button 
               onClick={handleCancel} 
@@ -21,7 +50,7 @@ const ModalCart = ({ title }) => {
             >
               Cancelar
             </button>
-            <button className="btnConfirmar">Confirmar Compra</button>
+            <button onClick={handleConfirm} className="btnConfirmar">Confirmar Compra</button>
           </div>
         </div>
       </div>
